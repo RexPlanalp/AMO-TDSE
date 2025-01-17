77719949a9f9e5b7c56eb52405c73c9b6235f1ba
PLANALP Donald Rex <dopl4670@multiphoton.colorado.edu>
2025-01-17 06:40:09 -0700

    seems to be working, now to work on matrix class

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import matplotlib
 matplotlib.use("Agg") 
 import matplotlib.pyplot as plt
 
-bspline_data = np.loadtxt("bsplines.txt")
-dbspline_data = np.loadtxt("dbsplines.txt")
+bspline_data = np.loadtxt("build/bsplines.txt")
+dbspline_data = np.loadtxt("build/dbsplines.txt")
 
 real = bspline_data[:,0]
 imag = bspline_data[:,1]
@@ -18,9 +18,9 @@ Nx = 3000
 for i in range(n_basis):
     plt.plot(real[i*Nx:(i+1)*Nx],color = "k")
     plt.plot(imag[i*Nx:(i+1)*Nx],color = "brown")
-plt.savefig("bspline.png")
+plt.savefig("build/bspline.png")
 plt.clf()
 for i in range(n_basis):
     plt.plot(dreal[i*Nx:(i+1)*Nx],color = "k")
     plt.plot(dimag[i*Nx:(i+1)*Nx],color = "brown")
-plt.savefig("dbspline.png")
+plt.savefig("build/dbspline.png")
