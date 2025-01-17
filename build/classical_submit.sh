110b946570d43c1341fba5cc432dcfdb9c12de7a
PLANALP Donald Rex <dopl4670@multiphoton.colorado.edu>
2025-01-17 13:08:38 -0700

    something wrong with matrix stuff, now is a good time to refactor

--- build/classical_submit.sh
+++ build/classical_submit.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 #SBATCH --job-name testing
 #SBATCH --output run.log
-#SBATCH --partition=compute --qos=normal
+#SBATCH --partition=compute 
 #SBATCH --exclude=photon13,photon11
 #SBATCH --nodes 1
-#SBATCH --ntasks 24
+#SBATCH --ntasks 2
 #SBATCH --mem=32G
 
-source /home/becker/dopl4670/.bashrc
+source ~/spack/share/spack/setup-env.sh
+
+spack load petsc
+spack load slepc
 
 REPO_DIR="/home/becker/dopl4670/Research/TDSE/build"
 hostname
--- source_files/main.cpp
+++ source_files/main.cpp
@@ -12,54 +12,36 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    std::cout << "TEST" << std::endl;
+    double start = MPI_Wtime();
     PetscErrorCode ierr;
     ierr = SlepcInitialize(&argc, &argv, NULL, NULL); CHKERRQ(ierr);
     int rank,size;
     MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
-    std::cout << "TEST" << std::endl;
-
-    // Define input file string
-    // std::string input_file = "input.json";
-
-
-    // bsplines basis(input_file);
-    
-    // basis.save_debug_info(rank);
-    // basis.save_debug_info_bsplines(rank);
-
-    // tise TISE(input_file);
-    // TISE.save_debug_info(rank);
-
-
-
-    // matrix S = TISE.compute_overlap_matrix("mpi",basis);
-
-   
-  
-    Mat S;
-    MatCreate(PETSC_COMM_WORLD, &S); 
-    MatSetSizes(S,PETSC_DECIDE,PETSC_DECIDE,100000,100000);
-    MatSetFromOptions(S);
-    MatMPIAIJSetPreallocation(S,1,NULL,1,NULL);
-    MatSetUp(S);
-    int start_row,end_row;
-    double start = MPI_Wtime();
-    MatGetOwnershipRange(S,&start_row,&end_row);
-    for (int i=start_row; i<end_row; i++) {
-        MatSetValue(S,i,i,1.0,INSERT_VALUES);
-    }
- 
-    MatAssemblyBegin(S,MAT_FINAL_ASSEMBLY);
-    MatAssemblyEnd(S,MAT_FINAL_ASSEMBLY);
     double end = MPI_Wtime();
     PetscPrintf(PETSC_COMM_WORLD,"Time taken: %f\n",end-start);
 
- 
+    //Define input file string
+    std::string input_file = "input.json";
 
-    
 
+    bsplines basis(input_file);
     
+    basis.save_debug_info(rank);
+    basis.save_debug_info_bsplines(rank);
+
+    tise TISE(input_file);
+    TISE.save_debug_info(rank);
+
+
+
+    matrix S = TISE.compute_overlap_matrix("mpi",basis);
+
+   
+    
+   
+   
     ierr = SlepcFinalize(); CHKERRQ(ierr);
     return 0;
 }
+
+
