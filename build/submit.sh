cc1f0f3f142fc48fb80629e59576d41f3917d8d8
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2025-01-23 22:55:30 -0700

    compiled petsc out of debug mode and its much faster, now i want to add timing inside main functions

--- build/submit.sh
+++ build/submit.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/bash
 #SBATCH --job-name testing
 #SBATCH --output run.log
-#SBATCH --partition=compute 
-#SBATCH --exclude=photon13,photon11
 #SBATCH --nodes 1
-#SBATCH --ntasks 24
-#SBATCH --mem=4G
+#SBATCH --ntasks 32
+#SBATCH --mem=8G
 
 source ~/spack/share/spack/setup-env.sh
 
 spack load petsc
 spack load slepc
 
-REPO_DIR="/home/becker/dopl4670/Research/TDSE/build"
+REPO_DIR="/users/becker/dopl4670/Research/TDSE_PETSC/build"
 hostname
 pwd
 
