Removed norm computation from block for now, also added cep fix from bejan to laser definition

observable.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use("Agg") 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
+import os
 
 import numpy as np
 import sys
@@ -10,11 +11,14 @@ import json
 with open("debug/debug.json") as f:
     data = json.load(f)
 
+if not os.path.exists("images"):
+    os.makedirs("images")
+
 if "BLOCK" in sys.argv:
     lmax = data["angular_data"]["lmax"]
     lm_to_block_txt = np.loadtxt("debug/lm_to_block.txt")
     probabilities_txt = np.loadtxt("TDSE_files/block_norms.txt")
-    fig,ax = plt.subplots()
+    fig,ax = plt.subplots(figsize=(10, 8))
     space_size =lmax + 1
     space = np.zeros((space_size, 2 * lmax + 1))
 
@@ -24,16 +28,20 @@ if "BLOCK" in sys.argv:
     column4 = probabilities_txt[:,0]
     column5 = probabilities_txt[:,1]
     for i in range(len(column1)):
-        print("Block Norm:", i, column4[i])
         space[lmax - int(column1[i]), int(column2[i]) + lmax] = column4[i]
 
     space[space==0] = np.min(space[space!=0])
-    cax =ax.imshow(np.flipud(space), cmap='inferno', interpolation='nearest', origin='lower',norm = LogNorm())
+    cax = ax.imshow(space, cmap='inferno', interpolation='nearest', norm=LogNorm())  
     ax.set_xlabel('m')
     ax.set_ylabel('l')
-    ax.set_xticks([i for i in range(0, 2 * lmax + 1, 10)])  # Positions for ticks
-    ax.set_xticklabels([str(i - lmax) for i in range(0, 2 * lmax + 1, 10)])  # Labels from -lmax to lmax
-    ax.set_title('Reachable (white) and Unreachable (black) Points in l-m Space')
+    ax.set_xticks([0, lmax, 2 * lmax])  
+    ax.set_xticklabels([-lmax, 0, lmax])
+    ax.set_yticks([0, lmax])  
+    ax.set_yticklabels([lmax, 0])  
+    ax.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False) 
 
     fig.colorbar(cax, ax=ax, shrink=0.5)
-    fig.savefig("block.png")
+    ax.set_title('Heatmap of Probabilities for l and m Values')
+    fig.savefig("images/pyramid.png")
+    fig.clf()
+