7bbc5efd02432abba668b114263efa8f97052d9c
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2025-02-11 13:02:53 -0700

    modularized more of tise code. Need to check for memory leaks, modularize saving a directory

--- src/tise.cpp
+++ src/tise.cpp
@@ -61,6 +61,63 @@ namespace tise
         return ierr;
     }
 
+    PetscErrorCode solve_eigenvalue_problem(const Mat& H, const Mat& S, EPS& eps, int num_of_energies, int& nconv)
+    {
+        PetscErrorCode ierr;
+        ierr = EPSSetOperators(eps,H,S); CHKERRQ(ierr);
+        ierr = EPSSetDimensions(eps,num_of_energies,PETSC_DEFAULT,PETSC_DEFAULT); CHKERRQ(ierr);
+        ierr = EPSSolve(eps); CHKERRQ(ierr);
+        ierr = EPSGetConverged(eps,&nconv); CHKERRQ(ierr);
+        return ierr;
+    }
+
+    PetscErrorCode extract_normalized_eigenvector(Vec& eigenvector,const EPS& eps, const Mat& S, int i)
+    {   
+        PetscErrorCode ierr;
+        ierr = MatCreateVecs(S,&eigenvector, NULL); CHKERRQ(ierr);
+        ierr = EPSGetEigenvector(eps,i,eigenvector,NULL); CHKERRQ(ierr);
+
+        std::complex<double> norm;
+        Vec y;
+        ierr = VecDuplicate(eigenvector,&y); CHKERRQ(ierr);
+        ierr = MatMult(S,eigenvector,y); CHKERRQ(ierr);
+        ierr = VecDot(eigenvector,y,&norm); CHKERRQ(ierr);
+        ierr = VecScale(eigenvector,1.0/std::sqrt(norm.real())); CHKERRQ(ierr);
+        return ierr;
+    }
+
+    PetscErrorCode save_eigenvalue(const std::complex<double>& eigenvalue, PetscViewer& viewTISE, int l, int i)
+    {   
+        PetscErrorCode ierr;
+        std::string eigenvalue_name = std::string("E_") + std::to_string(i+l+1) + '_' + std::to_string(l);
+        ierr = PetscViewerHDF5PushGroup(viewTISE,"/eigenvalues"); CHKERRQ(ierr);
+
+        Vec eigenvalue_vec;
+        ierr = VecCreate(PETSC_COMM_WORLD, &eigenvalue_vec); CHKERRQ(ierr);
+        ierr = VecSetSizes(eigenvalue_vec,PETSC_DECIDE,1); CHKERRQ(ierr);
+        ierr = VecSetFromOptions(eigenvalue_vec); CHKERRQ(ierr);
+        ierr=  VecSetValue(eigenvalue_vec,0,eigenvalue,INSERT_VALUES); CHKERRQ(ierr);
+        ierr = VecAssemblyBegin(eigenvalue_vec); CHKERRQ(ierr);
+        ierr = VecAssemblyEnd(eigenvalue_vec); CHKERRQ(ierr);
+    
+        ierr = PetscObjectSetName((PetscObject)eigenvalue_vec,eigenvalue_name.c_str()); CHKERRQ(ierr);
+        ierr = VecView(eigenvalue_vec, viewTISE); CHKERRQ(ierr);
+        ierr = PetscViewerHDF5PopGroup(viewTISE); CHKERRQ(ierr);
+        ierr = VecDestroy(&eigenvalue_vec); CHKERRQ(ierr);
+
+        return ierr;
+    }
+
+    PetscErrorCode save_eigenvector(Vec& eigenvector, PetscViewer& viewTISE, int l, int i)
+    {
+        PetscErrorCode ierr;
+        std::string eigenvector_name = std::string("psi_") + std::to_string(i+l+1) + "_" + std::to_string(l);
+        ierr = PetscViewerHDF5PushGroup(viewTISE, "/eigenvectors"); CHKERRQ(ierr);
+        ierr = PetscObjectSetName((PetscObject)eigenvector,eigenvector_name.c_str()); CHKERRQ(ierr);
+        ierr = VecView(eigenvector,viewTISE); CHKERRQ(ierr);
+        ierr = PetscViewerHDF5PopGroup(viewTISE); CHKERRQ(ierr);
+        return ierr;
+    }
 
     PetscErrorCode solve_tise(const simulation& sim,int rank)
     {   
@@ -103,11 +160,6 @@ namespace tise
 
         PetscPrintf(PETSC_COMM_WORLD, "Setting Up Eigenvalue Problem  \n\n");
         EPS eps;
-        // ierr = EPSCreate(PETSC_COMM_WORLD, &eps); CHKERRQ(ierr);
-        // ierr = EPSSetProblemType(eps, EPS_GNHEP); CHKERRQ(ierr);
-        // ierr = EPSSetWhichEigenpairs(eps, EPS_SMALLEST_REAL); CHKERRQ(ierr);
-        // ierr = EPSSetType(eps,EPSKRYLOVSCHUR); CHKERRQ(ierr);
-        // ierr = EPSSetTolerances(eps,config.tise_tolerance,config.tise_mat_iter); CHKERRQ(ierr);
         ierr = setup_eigenvalue_problem(config,eps); CHKERRQ(ierr);
 
 
@@ -126,10 +178,7 @@ namespace tise
                 continue;
             }
 
-            ierr = EPSSetOperators(eps,temp,S); CHKERRQ(ierr);
-            ierr = EPSSetDimensions(eps,num_of_energies,PETSC_DEFAULT,PETSC_DEFAULT); CHKERRQ(ierr);
-            ierr = EPSSolve(eps); CHKERRQ(ierr);
-            ierr = EPSGetConverged(eps,&nconv); CHKERRQ(ierr);
+            ierr = solve_eigenvalue_problem(temp,S,eps,num_of_energies,nconv); CHKERRQ(ierr);
             PetscPrintf(PETSC_COMM_WORLD, "Eigenvalues Requested %d, Eigenvalues Converged: %d \n\n", num_of_energies,nconv); CHKERRQ(ierr);
 
             for (int i = 0; i < nconv; ++i)
@@ -145,52 +194,22 @@ namespace tise
                     continue;
                 }
 
-
-                std::string eigenvalue_name = std::string("E_") + std::to_string(i+l+1) + '_' + std::to_string(l);
-                ierr = PetscViewerHDF5PushGroup(viewTISE,"/eigenvalues"); CHKERRQ(ierr);
-
-                Vec eigenvalue_vec;
-                ierr = VecCreate(PETSC_COMM_WORLD, &eigenvalue_vec); CHKERRQ(ierr);
-                ierr = VecSetSizes(eigenvalue_vec,PETSC_DECIDE,1); CHKERRQ(ierr);
-                ierr = VecSetFromOptions(eigenvalue_vec); CHKERRQ(ierr);
-                ierr=  VecSetValue(eigenvalue_vec,0,eigenvalue,INSERT_VALUES); CHKERRQ(ierr);
-                ierr = VecAssemblyBegin(eigenvalue_vec); CHKERRQ(ierr);
-                ierr = VecAssemblyEnd(eigenvalue_vec); CHKERRQ(ierr);
-
-                ierr = PetscObjectSetName((PetscObject)eigenvalue_vec,eigenvalue_name.c_str()); CHKERRQ(ierr);
-                ierr = VecView(eigenvalue_vec, viewTISE); CHKERRQ(ierr);
-                ierr = PetscViewerHDF5PopGroup(viewTISE); CHKERRQ(ierr);
-                ierr = VecDestroy(&eigenvalue_vec); CHKERRQ(ierr);
+                ierr = save_eigenvalue(eigenvalue,viewTISE,l,i); CHKERRQ(ierr);
 
                 Vec eigenvector;
-                ierr = MatCreateVecs(temp,&eigenvector, NULL); CHKERRQ(ierr);
-                ierr = EPSGetEigenvector(eps,i,eigenvector,NULL); CHKERRQ(ierr);
-
+                ierr = extract_normalized_eigenvector(eigenvector,eps,S,i); CHKERRQ(ierr);
+                
                 std::complex<double> norm;
-                Vec y;
-                ierr = VecDuplicate(eigenvector,&y); CHKERRQ(ierr);
-                ierr = MatMult(S,eigenvector,y); CHKERRQ(ierr);
-                ierr = VecDot(eigenvector,y,&norm); CHKERRQ(ierr);
-                ierr = VecScale(eigenvector,1.0/std::sqrt(norm.real())); CHKERRQ(ierr);
-
-                ierr = MatMult(S,eigenvector,y); CHKERRQ(ierr);
-                ierr = VecDot(eigenvector,y,&norm); CHKERRQ(ierr);
-
+                Vec temp_vec;
+                ierr = VecDuplicate(eigenvector,&temp_vec); CHKERRQ(ierr);
+                ierr = MatMult(S,eigenvector,temp_vec); CHKERRQ(ierr);
+                ierr = VecDot(eigenvector,temp_vec,&norm); CHKERRQ(ierr);
+                norm = std::sqrt(norm);
                 if (sim.debug)
                 {
                     PetscPrintf(PETSC_COMM_WORLD,"Eigenvector %d -> Norm(%.4f , %.4f) -> Eigenvalue(%.4f , %.4f)  \n",i+1,norm.real(),norm.imag(),eigenvalue.real(),eigenvalue.imag()); CHKERRQ(ierr);
                 }
-                
-
-                std::string eigenvector_name = std::string("psi_") + std::to_string(i+l+1) + "_" + std::to_string(l);
-                ierr = PetscViewerHDF5PushGroup(viewTISE, "/eigenvectors"); CHKERRQ(ierr);
-                ierr = PetscObjectSetName((PetscObject)eigenvector,eigenvector_name.c_str()); CHKERRQ(ierr);
-                ierr = VecView(eigenvector,viewTISE); CHKERRQ(ierr);
-                ierr = PetscViewerHDF5PopGroup(viewTISE); CHKERRQ(ierr);
-
-                ierr = VecDestroy(&eigenvector); CHKERRQ(ierr);
-                ierr = VecDestroy(&y); CHKERRQ(ierr);
-
+                ierr = save_eigenvector(eigenvector,viewTISE,l,i); CHKERRQ(ierr);
             }
             ierr = MatDestroy(&temp); CHKERRQ(ierr);
 
